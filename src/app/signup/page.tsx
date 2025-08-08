85aee28e8fba808b69a42e0e3c48e0a8957c2c7a
marvcom <marvinjameserosa@gmail.com>
2025-08-08 08:17:58 +0800

    feat: change metadata

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,4 +1,9 @@
 import SignUp from "@/components/sections/SignUp";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign Up | Lunoa",
+};
 
 export default function VerifyBusinessPartnerPage() {
   return <SignUp />;
